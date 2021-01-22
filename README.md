3d3049261204a98a9ccd5f88341aee5e9b618967
aryan26roy <aryanroy5678@gmail.com>
2021-01-22 11:52:01 +0530

    Added new files

---
+++ README.md
@@ -0,0 +1,72 @@
+# PATHFINDERS
+A library containing pathfinding algorithms implemented and compiled in C++ and binded and exposed to Python. It enabled fast execution of these pathfinding algorithms in Python.
+ 
+ ## How To Install
+ To install the library, simply clone this repository and go to the folder where it has been cloned and type the following commands.
+ ```bash
+ cmake -S . -B build
+ cmake --build build
+ ```
+ ## How To Use
+ To use the library simply import the wrapper class from the folder where it was built:
+ ```python
+ import pathfinders
+ ```
+ Then the class can be instantiated like this:
+ ```python
+ finder = pathfinders.pathfinder()
+ ```
+ ### Inputing A Map
+ In the pathfinders library there are two ways to input a map.
+ * Rasterized Form
+ * Obstacles form
+
+To input the map in the rasterized form, assuming the map is in 2-Dimensional form in the variable map:
+```python
+finder.mapras(map)
+```
+To input the map in the obstacles form, a 2D list of the coordinates of the obstacles on the map need to be provided along with the shape and the start and stop points:
+```python
+finder.map(obs,dims,start,stop)
+```
+### Map Manipulation
+The library provides two simple functions to modify the map, they are:
+* Mark
+* Delete
+  
+The Mark function can be used to mark a new set of start and stop points:
+```python
+finder.mark(start,stop)
+```
+The delete function can be used to delete the current start and stop points on the map:
+```python
+finder.delete()
+```
+### Finding The Shortest Path
+Once the start and stop points have been set, the findpath function can be used to find the shortest path between them.
+The function returns the path as an Awkward Record Array which enables the user to manipulate the path using the functionalities provided by [Awkward Array Library](https://github.com/scikit-hep/awkward-1.0).
+The different algorithms provided in the library are:
+* Breadth-First Search
+* Depth-First Search
+* Dijkstra's Algorithm
+* A-Star Algorithm (To be added)
+
+For Breadth-First Search:
+```python
+path = finder.findpath("BFS")
+```
+For Depth-First Search:
+```python
+path = finder.findpath("DFS")
+```
+For Dijkstra's Algorithm:
+```python
+path = finder.findpath("DJK")
+```
+## Dependencies
+The library has the following python dependencies:
+* Awkward Array
+* Numpy
+
+
+
---
+++ src/pathfinders/__init__.py
@@ -0,0 +1,102 @@
+from __future__ import absolute_import
+import sys
+
+from . import _core
+import awkward as ak
+
+
+class pathfinder:       # wrapper class on python side
+    def map(self, obs, dims, start, stop):
+        for elem in obs:
+            if elem[0] > dims[0] or elem[1] > dims[1]:
+                print("Obstacle Outside Map")
+                raise ValueError
+        if start[0] > dims[0] or start[1] > dims[1] or stop[0] > dims[0] or stop[1] > dims[1]:
+            print("Start/Stop Outside Map")
+            raise ValueError
+        if start in obs or stop in obs:
+            print("Can't mark obstacles")
+            raise ValueError
+        grid = []
+        for i in range(dims[0]):
+            aee = []
+            for j in range(dims[1]):
+                if [i, j] in obs:
+                    aee.append(0)
+                    continue
+                if [i, j] == start:
+                    aee.append(2)
+                    continue
+                if [i, j] == stop:
+                    aee.append(1)
+                    continue
+                else:
+                    aee.append(4)
+            grid.append(aee)
+        for elem in grid:
+            print(elem)
+        self.grid = grid
+
+    def mapras(self, grid):
+        lg = len(grid[0])
+        for elem in grid:
+            if len(elem) != lg:
+                print("Dimension Mismatch")
+                raise ValueError
+        self.grid = grid        # the map
+        self.rows = len(grid)       # metadata of the map
+        self.cols = len(grid[0])
+
+    def mark(self, start, stop):        # function to mark the start and stop point on the map
+        if start[0] > self.rows or start[1] > self.cols or start[0] > self.rows or start[1] > self.cols:
+            print("Start/Stop outside the map")
+            raise ValueError
+        count = 0
+        for elem in self.grid:      # raise exception if there is no start/stop
+            if 1 in elem:
+                count += 1
+            if 2 in elem:
+                count += 1
+        if count == 2:
+            	raise ValueError
+        if self.grid[start[0]][start[1]] == 0 or self.grid[stop[0]][stop[1]] == 0:
+            return print("Error: Cannot Mark Obstacles")
+        for i in range(self.rows):
+    	    for j in range(self.cols):
+    	        if i == start[0] and j == start[1]:
+    		        self.grid[i][j] = 2
+    	        if i == stop[0] and j == stop[1]:
+    		        self.grid[i][j] = 1
+        for elem in self.grid:
+            print(elem)
+
+    def delete(self):       # function to delete the start and stop points
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.grid[i][j] == 2 or self.grid[i][j] == 1:
+                    self.grid[i][j] =4
+        print(self.grid)
+
+    def findpath(self, algo):
+        count = 0
+        for elem in self.grid:      # raise exception if there is no start/stop
+            if 1 in elem:
+            	count += 1
+            if 2 in elem:
+            	count += 1
+        if count < 2 or count > 2:
+            raise ValueError
+        if algo == "BFS":
+            f = _core.BFS_FIND(self.grid)       # call the BFS algorithm
+        if algo == "DFS":
+            f = _core.DFS_FIND(self.grid)       # call the DFS algorithm
+        if algo == "DJK":
+            f = _core.interface(self.grid)     # call the Dijkstra Algorithm
+        form1 = list(f.keys())[-1]
+        f.pop(form1)
+        length1 = f['Length'][0]
+        f.pop('Length')
+        for key in f:
+            f[key] = f[key].astype('int64')
+        aa = ak.from_buffers(form1, length1, f)
+        return aa
